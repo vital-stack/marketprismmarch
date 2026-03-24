3cfdda5441a2e83c263338ddf7bf381ce3fa5c04
vital-stack <tara@vtlbranding.com>
2026-03-24 17:33:06 -0400

    Update blog.js

--- api/blog.js
+++ api/blog.js
@@ -2,6 +2,9 @@ const { readFileSync } = require('fs');
 const { join } = require('path');
 
 module.exports = (req, res) => {
+  const supabaseUrl  = process.env.SUPABASE_URL  || '';
+  const supabaseAnon = process.env.SUPABASE_ANON || '';
+
   let html;
   try {
     html = readFileSync(join(__dirname, '_blog.html'), 'utf8');
@@ -9,7 +12,11 @@ module.exports = (req, res) => {
     html = readFileSync(join(process.cwd(), '_blog.html'), 'utf8');
   }
 
+  html = html.replace(
+    "window.__env = { SUPABASE_URL: '', SUPABASE_ANON: '' };",
+    `window.__env = { SUPABASE_URL: '${supabaseUrl}', SUPABASE_ANON: '${supabaseAnon}' };`
+  );
+
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=3600');
   res.status(200).send(html);
 };
